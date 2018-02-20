45af83db9fd0bc0677520b4562d878718a3a3082
Hermoso <ezequiel.m.hermoso@accenture.com>
2018-02-20 12:01:03 -0300

    Update Azure

--- azure-services-blob.ts
+++ azure-services-blob.ts
@@ -55,7 +55,12 @@ export class AzureStorageService {
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 // En el environment de angular vamos a declarar la configuracion para la conexion con Azure
+
+
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
