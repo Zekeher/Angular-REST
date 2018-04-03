856c18386cda8295615b52d1a3a7c7e34f29c084
Hermoso, Ezequiel M <ezequiel.m.hermoso@accenture.com>
2018-04-03 14:44:19 -0300

    Update Model UploadFile

--- angular-example/src/app/content/drop-file/drop-file.component.ts
+++ angular-example/src/app/content/drop-file/drop-file.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UploadFile } from '../../models/UploadFile';
+import { UploadFile } from './models/UploadFile';
 import * as _ from "lodash";
 
 @Component({
